60559a9b7571a0625a6ac0a043b6ed703d2a32af
lap <2959452323@qq.com>
2023-09-22 14:23:44 +0800

    add class HousePrice, delete node.cpp

---
+++ HousePrice.cpp
@@ -0,0 +1,5 @@
+//
+// Created by He stu on 2023/9/22.
+//
+
+#include "HousePrice.h"
---
+++ HousePrice.h
@@ -0,0 +1,14 @@
+//
+// Created by He stu on 2023/9/22.
+//
+
+#ifndef OOPCES_HOUSEPRICE_H
+#define OOPCES_HOUSEPRICE_H
+
+
+class HousePrice {
+
+};
+
+
+#endif //OOPCES_HOUSEPRICE_H
